8aff53f90ab4e3e1e94874a7a6036f1e8efec6d0
Colby McHenry <me@colbymchenry.com>
2022-03-08 12:15:51 -0500

    Added important packages.

---
+++ util/firebase.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtpwuNWUE1LN-kWlwMANGLoFg_hVnxlzU",
+    authDomain: "hydromade-io.firebaseapp.com",
+    projectId: "hydromade-io",
+    storageBucket: "hydromade-io.appspot.com",
+    messagingSenderId: "784118829449",
+    appId: "1:784118829449:web:b503603308e21049ead158"
+};
