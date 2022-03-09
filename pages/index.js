5f3544b13cd9cd355146c5e203745fc649c4c75a
Colby McHenry <me@colbymchenry.com>
2022-03-08 23:18:59 -0500

    Fixed import.

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 import { useState } from 'react';
 import CreateAccountModal from '../components/CreateAccountModal';
+import { toggleFullscreenScroll } from '../utils/modal-utils';
 
 export default function Home() {
 
