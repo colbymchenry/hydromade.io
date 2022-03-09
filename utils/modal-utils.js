beb25a34968480999fe819ef28b9191315d7e82a
Colby McHenry <me@colbymchenry.com>
2022-03-08 23:00:25 -0500

    Final push.

--- utils/modal-utils.js
+++ utils/modal-utils.js
@@ -2,5 +2,7 @@ export function toggleFullscreenScroll(on) {
     if (typeof document !== 'undefined') {
         document.getElementById("__next").style.height = !on ? "100vh" : "unset"
         document.getElementById("__next").style.overflow = !on ? "hidden" : "unset"
+        document.getElementById("root").style.height = !on ? "100%" : "unset"
+        document.getElementById("root").style.overflow = !on ? "hidden" : "unset"
     }
 }
