6600cf72e1ac3248dcf882a8eb15b9ba87fea4ed
Colby McHenry <me@colbymchenry.com>
2022-03-09 01:36:24 -0500

    Way better animation timing.

--- components/CreateAccountModal/index.js
+++ components/CreateAccountModal/index.js
@@ -16,23 +16,23 @@ export default function CreateAccountModal(props) {
     }, [])
 
     return (
-        <animated.div style={close ? GoOutOfView(1400, () => props.close()) : ComeIntoView()} className={styles.container}>
+        <animated.div style={close ? GoOutOfView(800, () => props.close()) : ComeIntoView()} className={styles.container}>
             <button type="button" onClick={() => setClose(true)}><FontAwesomeIcon icon={faX} /></button>
             <div>
-                <animated.div style={close ? FadeOutDown(1100) : FadeInUp(500)} className={`w-100 ${styles.margin1}`}>
+                <animated.div style={close ? FadeOutDown(500) : FadeInUp(200)} className={`w-100 ${styles.margin1}`}>
                     <input type="email" placeholder="me@example.com" />
                     <div className={styles.label}><small>Email</small></div>
                 </animated.div>
-                <animated.div style={close ? FadeOutDown(800) : FadeInUp(800)} className={`w-100 ${styles.margin2}`}>
+                <animated.div style={close ? FadeOutDown(400) : FadeInUp(300)} className={`w-100 ${styles.margin2}`}>
                     <input type="password" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" />
                     <div className={styles.label}><small>Password (at least 8 characters)</small></div>
                 </animated.div>
-                <animated.div style={close ? FadeOutDown(500) : FadeInUp(1100)} className={`w-100 ${styles.margin3}`}>
+                <animated.div style={close ? FadeOutDown(300) : FadeInUp(400)} className={`w-100 ${styles.margin3}`}>
                     <input type="password" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" />
                     <div className={styles.label}><small>Password Confirmation</small></div>
                 </animated.div>
 
-                <animated.div style={close ? FadeOutDown(200) : FadeInUp(1400)} className={`w-100 d-flex justify-content-center ${styles.margin4}`}>
+                <animated.div style={close ? FadeOutDown(200) : FadeInUp(500)} className={`w-100 d-flex justify-content-center ${styles.margin4}`}>
                     <ButtonSubmit label="Create Account" processing={false} />
                 </animated.div>
             </div>
