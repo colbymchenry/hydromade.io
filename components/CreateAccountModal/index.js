c0dc52a7fdb2fc6f0cf3b1b07451b9b982a8c9e2
Colby McHenry <me@colbymchenry.com>
2022-03-08 23:08:31 -0500

    Make leaving modal faster.

--- components/CreateAccountModal/index.js
+++ components/CreateAccountModal/index.js
@@ -12,28 +12,28 @@ export default function CreateAccountModal(props) {
     const [close, setClose] = useState(false);
 
     useEffect(() => {
-     toggleFullscreenScroll(false)   
+        toggleFullscreenScroll(false)
     }, [])
 
     return (
-        <animated.div style={close ? GoOutOfView(2000, () => props.close()) : ComeIntoView()} className={styles.container}>
+        <animated.div style={close ? GoOutOfView(1400, () => props.close()) : ComeIntoView()} className={styles.container}>
             <button type="button" onClick={() => setClose(true)}><FontAwesomeIcon icon={faX} /></button>
             <div>
-                <animated.div style={close ? FadeOutDown(1800) : FadeInUp(1000)} className={`w-100 ${styles.margin1}`}>
+                <animated.div style={close ? FadeOutDown(1100) : FadeInUp(1000)} className={`w-100 ${styles.margin1}`}>
                     <input type="email" placeholder="me@example.com" />
                     <div className={styles.label}><small>Email</small></div>
                 </animated.div>
-                <animated.div style={close ? FadeOutDown(1500) : FadeInUp(1200)} className={`w-100 ${styles.margin2}`}>
+                <animated.div style={close ? FadeOutDown(800) : FadeInUp(1200)} className={`w-100 ${styles.margin2}`}>
                     <input type="password" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" />
                     <div className={styles.label}><small>Password (at least 8 characters)</small></div>
                 </animated.div>
-                <animated.div style={close ? FadeOutDown(1200) : FadeInUp(1500)} className={`w-100 ${styles.margin3}`}>
+                <animated.div style={close ? FadeOutDown(500) : FadeInUp(1500)} className={`w-100 ${styles.margin3}`}>
                     <input type="password" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" />
                     <div className={styles.label}><small>Password Confirmation</small></div>
                 </animated.div>
 
-                <animated.div style={close ? FadeOutDown(1000) : FadeInUp(1800)} className={`w-100 d-flex justify-content-center ${styles.margin4}`}>
-                    <ButtonSubmit label="Create Account" processing={false} />                
+                <animated.div style={close ? FadeOutDown(200) : FadeInUp(1800)} className={`w-100 d-flex justify-content-center ${styles.margin4}`}>
+                    <ButtonSubmit label="Create Account" processing={false} />
                 </animated.div>
             </div>
         </animated.div>
