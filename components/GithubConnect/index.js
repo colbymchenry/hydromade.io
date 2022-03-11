5a87a1f64efdb5784a9b7f5a030f41f2c95f2e67
Colby McHenry <me@colbymchenry.com>
2022-03-11 14:54:35 -0500

    Creating themes and linking github.

---
+++ components/GithubConnect/index.js
@@ -0,0 +1,15 @@
+import styles from './styles.module.css'
+
+export default function StripeConnect(props) {
+
+    return (
+        <div className={"d-flex flex-column " + styles.main}>
+            {props.requirements && props.requirements.includes("individual.verification.document") ?
+                <h3>Your Stripe account requires documents for verification.</h3> :
+                <h3>To upload a theme you must create a Stripe account.</h3>
+            }
+            <a href={props.url}>{(props.requirements && props.requirements.length) ? "Finish Setup" : "Create Account"}</a>
+        </div>
+    )
+
+}
