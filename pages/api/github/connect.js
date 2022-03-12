be8337509a504a953a2d22ec3b0c7e372048269e
Colby McHenry <colbymchenry@gmail.com>
2022-03-12 17:55:49 -0500

    Gotta start printing shit.

--- pages/api/github/connect.js
+++ pages/api/github/connect.js
@@ -7,10 +7,19 @@ export default async function handler(req, res) {
 
     if (!req.query?.code || !req.query?.uid) return res.status(400).json({});
 
+    console.log(req.query)
+
     try {
         let user = await getUserByUID(req.query.uid);
 
         if (!user) return res.status(400).json({});
+
+        console.log({
+            client_id: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+            client_secret: process.env.GITHUB_CLIENT_SECRET,
+            code: req.query.code,
+            redirect_uri: `${process.env.APP_URL}/app`
+        })
 
         const tokenResponse = await axios.post("https://github.com/login/oauth/access_token", {
             client_id: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
